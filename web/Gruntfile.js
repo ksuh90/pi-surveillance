1b34b9d312300995f0704a0d3d956ad4b9306eb8
ksuh <k900209@gmail.com>
2016-04-07 05:51:24 -0400

    tidy up gruntfile and package

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -1,5 +1,3 @@
-
-// grunt entry point
 module.exports = function(grunt) {
 
     grunt.initConfig({
@@ -25,75 +23,8 @@ module.exports = function(grunt) {
                     '<%= dest.js.main %>': [
                         'scripts/components/*.jsx',
                     ],
-
-
                 }
             }
-        },
-
-        concat: {
-            options: {
-                separator: '\r\n',
-            },
-
-
-            /*
-             * CSS
-             */
-/*
-            
-
-            main_css: {
-                src: [
-                    'css/fonts.css',
-                    'css/lib/bootstrap/3.3.5/bootstrap.min.css',
-                    'css/lib/dropzone.css',
-                    'css/lib/filepicker.css',
-                    'css/lib/spinner.css',
-                    'css/main.style.css'
-                ],
-                dest: '<%= pkg.dest.css %>main.style.css'
-            },
-
-            download_css: {
-                src: [
-                    'css/lib/bootstrap/3.3.5/bootstrap.min.css',
-                    'css/intro.style.css',
-                    'css/download.style.css'
-                ],
-                dest: '<%= pkg.dest.css %>download.style.css'
-            },
-*/
-
-
-
-            /*
-             * Javascript
-             */
-
-            main_js: {
-                src: [
-                    'scripts/lib/jquery.min.js',
-
-                    '<%= dest.js.main %>'
-                ],
-                dest: '<%= dest.js.main %>'
-            },
-
-        },
-
-
-        cssmin: {
-            styles: {
-                options: {
-                    banner: '/*! <%= grunt.template.today("mm-dd-yyyy") %> */\n'
-                },                
-                files: {
-                    //'<%= concat.intro_css.dest %>': ['<%= concat.intro_css.dest %>'],
-                    //'<%= concat.main_css.dest %>': ['<%= concat.main_css.dest %>'],
-                    //'<%= concat.download_css.dest %>': ['<%= concat.download_css.dest %>']
-                },
-            },
         },
 
         uglify: {
@@ -108,42 +39,14 @@ module.exports = function(grunt) {
                 }
             }
         },
-
-
-        cacheBust: {
-            taskName: {
-                options: {
-                    separator: '-',
-                    deleteOriginals: true,
-                    assets: ['scripts/dist/*', 'css/dist/*'],
-                },
-                src: ['<%= pkg.dest.module %>Assets.php']
-            }
-        },
-
-
     });
 
 
-
     grunt.loadNpmTasks('grunt-browserify');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-
     grunt.loadNpmTasks('grunt-contrib-uglify');
-
-
-    grunt.loadNpmTasks('grunt-cache-bust');
-    //grunt.loadNpmTasks('grunt-contrib-watch');
-    
-
 
     grunt.registerTask('default', [
         'browserify',
-        //'concat',
-        //'cssmin',
         //'uglify',
-        //'cacheBust'
-        //'watch'
     ]);
 };
