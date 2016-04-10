5a68a63c04f652da494e729cb49c6a68dc40aa6d
ksuh <k900209@gmail.com>
2016-04-09 23:39:43 -0400

    refactoring control buttons

--- web/scripts/components/ControlPanel.jsx
+++ web/scripts/components/ControlPanel.jsx
@@ -4,33 +4,61 @@ var StatusAlert = require('./StatusAlert.jsx');
 
 /*
 [props]
-    app    
-*/   
+    app
+*/
 module.exports = React.createClass({
 
     render: function() {       
         var that = this;
-        
         var controls = [
             {
-                option: 'take_pic',
                 label: 'Take picture',
-                desc: 'Manually take a picture.'
+                desc: 'Manually take a picture.',
+                buttons: [
+                    {
+                        option: 'take_pic',
+                        label: 'Take picture'
+                    }
+                ]
             },
             {
-                option: 'pause',
-                label: 'Pause',
-                desc: 'Briefly pause the system.'
+                label: 'Pause/Resume',
+                desc: 'Pause/Resume the system.',
+                buttons: [
+                    {
+                        option: 'pause',
+                        label: 'Pause'
+                    },
+                    {
+                        option: 'resume',
+                        label: 'Resume'
+                    }
+                ]
             },
             {
-                option: 'resume',
-                label: 'Resume',
-                desc: 'Resume paused system.'
+                label: 'Email Notifications',
+                desc: 'Toggle email notifications',
+                buttons: [
+                    {
+                        option: 'email_on',
+                        label: 'On'
+                    },
+                    {
+                        option: 'email_off',
+                        label: 'Off'
+                    }
+                ]
+
             },
             {
-                option: 'ping',
                 label: 'Ping',
-                desc: 'Ping the pi-surveillance system'
+                desc: 'Ping the pi-surveillance system',
+                buttons: [
+                    {
+                        option: 'ping',
+                        label: 'Ping'
+                    }
+                ]
             }
         ];
         var rows = $.map(controls, function(row, key){
@@ -50,13 +78,36 @@ var Control = React.createClass({
     getInitialState: function() {
         return { statusComponent: ''};
     },
-    componentDidMount: function() {
+    controlRequest: function(payload) {
+        // publish control option via pubnub
         var that = this;
-        
+        that.props.app.pubnub.publish({
+            channel: that.props.app.pubnub_channel,
+            message: payload,
+            callback : function(m){
+                console.log('Control : pubnub : publish :');
+                console.log(m);
+            }
+        });
+    },
+    controlClick: function(option) {
+        var that = this;
+        console.log('remote control option: '+option);
+        this.setState({statusComponent: ''});
+        $.blockUI.defaults.message = '';
+        $.blockUI();
+
+        this.controlRequest({
+            type: 'control_request',
+            option: option
+        });
+    },
+    render: function() {
+        var that = this;
         that.props.app.pubnub.subscribe({
             channel: that.props.app.pubnub_channel,
             message: function(r){
-                console.log('ControlPanel : ');
+                console.log('ControlPanel: ');
                 console.log(r);
                 if (r.type == 'control_resp') {
                     var status = <StatusAlert status={true} msg={r.msg} />;
@@ -70,42 +121,32 @@ var Control = React.createClass({
                 //console.log(JSON.stringify(error));
             }
         });
-        
-    },
-    controlClick: function(option) {
-        var that = this;
 
-        console.log('remote control option: '+option);
-        that.setState({statusComponent: ''});
-        $.blockUI.defaults.message = '';
-        $.blockUI();
+        var buttons = $.map(this.props.row.buttons, function(button, key){
+            return (
+                <ControlButton key={key} btnData={button} controlClick={that.controlClick} />
+            );
+        });
 
-        // publish control option via pubnub
-        var payload = {
-            type: 'control_request',
-            option: option
-        };
-        that.props.app.pubnub.publish({
-            channel: that.props.app.pubnub_channel,        
-            message: payload,
-            callback : function(m){
-                console.log('Control : pubnub : publish :');
-                console.log(m);
-            }
-        });    
-    },
-    render: function() {
         return(
             <div className="jumbotron control-slot">
                 <h3>{this.props.row.label}</h3>
                 <p>{this.props.row.desc}</p>
                 <div className="status-container">{this.state.statusComponent}</div>
-                <button
-                    type="button"
-                    className="btn btn-lg btn-primary btn-control"
-                    onClick={this.controlClick.bind(null, this.props.row.option)}
-                    data-option={this.props.row.option}>{this.props.row.label}</button>
+                {buttons}
             </div>
+        );
+    }
+});
+
+var ControlButton = React.createClass({
+    render: function() {
+        return(
+            <button
+                type="button"
+                className="btn btn-lg btn-primary btn-control"
+                onClick={this.props.controlClick.bind(null, this.props.btnData.option)}
+                data-option={this.props.btnData.option}>{this.props.btnData.label}</button>
         );
     }
 });
