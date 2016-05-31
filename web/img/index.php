2e418ce73130ac770071af08b77f7389cb03b7c4
ksuh <k900209@gmail.com>
2016-05-31 03:54:53 -0400

    attempting to fix img 500 error

--- web/img/index.php
+++ web/img/index.php
@@ -1,15 +1,22 @@
 <?php
 //require_once '../../../../pi-surveillance-module/bootstrap.php';
 require_once '../../../pi-surveillance-module/bootstrap.php';
+require_once $config['PATH_MODULE'] . 'Sag/Sag.php';
 
 if (!isset($_GET['id']) || !isset($_GET['f'])) exit;
 
 $id = $_GET['id'];
 $fname = $_GET['f'];
-$auth = $config['CLOUDANT_API_KEY'].':'.$config['CLOUDANT_API_PASS'];
-$host = $config['CLOUDANT_HOST'];
-$db_name = $config['CLOUDANT_DB_NAME'];
-$url = "https://{$auth}@{$host}/{$db_name}/{$id}/{$fname}";
-$content = file_get_contents($url);
-header('Content-Type: image/jpeg');
-echo $content;
+
+$sag = new Sag($config['CLOUDANT_HOST'], '443');
+$sag->login($config['CLOUDANT_API_KEY'], $config['CLOUDANT_API_PASS']);
+$sag->useSSL(true);
+$sag->setDatabase($config['CLOUDANT_DB_NAME']);
+$sag->decode(true);
+
+$resp = $sag->get(urlencode($id) . '/' . urlencode($fname));
+$contentType = $resp->headers->{"content-type"};
+
+header('Content-Type: $contentType');
+echo $resp->body;
+exit;
